e8fda4348722371872ef689a17311b2c4465d3bc
james-work-account <31282758+james-work-account@users.noreply.github.com>
2020-10-15 22:16:07 +0100

    styling

--- src/components/Favourite.js
+++ src/components/Favourite.js
@@ -11,9 +11,9 @@ const Favourite = ({toggleFavourite, jokeState}) => {
         <button className={`favourite${style}`} onClick={toggleFavourite}>
             <span><i className="fas fa-star"></i></span>
             <span className="text">
-                {jokeState === JokeStates.Favourite ? "Remove from Favourites" : ""}
+                {jokeState === JokeStates.Favourite ? "Unfavourite" : ""}
                 {jokeState === JokeStates.RemoveFavouriteAreYouSure ? "Are you sure?" : ""}
-                {jokeState === JokeStates.NotFavourite ? "Add to Favourites" : ""}
+                {jokeState === JokeStates.NotFavourite ? "Favourite" : ""}
             </span>
         </button>
     )
